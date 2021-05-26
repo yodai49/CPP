4c805e021c237a953b65b838db989909a61fcfd1
yodai49 <82998022+yodai49@users.noreply.github.com>
2021-05-26 23:12:36 +0900

    Add files via upload

--- sample.js
+++ sample.js
@@ -3,6 +3,6 @@ $("#btn1").on("click",function(){
     let year = date.getFullYear();
     let month = date.getMonth()+1;
     let day = date.getDate();
-    let message = '今日は${year}年${month}月${day}日です';
+    let message = `今日は${year}年${month}月${day}日です`;
     $("#tBox").val(message);
 })
